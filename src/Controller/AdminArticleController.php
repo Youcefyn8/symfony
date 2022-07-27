3184c4899fa38e9eee1e967c6a1d819e37e15974
youcefyn8 <youcef.nemmiche@gmail.com>
2022-07-27 15:16:07 +0200

    symfo

---
+++ src/Controller/AdminArticleController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use App\Repository\WriterRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+#[Route('/admin/article')]
+class AdminArticleController extends AbstractController
+{
+    private WriterRepository $writerRepository;
+
+    public function __construct(WriterRepository $writerRepository)
+    {
+        // récupération du writer repository (grâce à l'auto wiring ci-dessus)
+        $this->writerRepository = $writerRepository;
+    }
+
+    #[Route('/', name: 'app_admin_article_index', methods: ['GET'])]
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        // les utilisateurs non authentifiés sont renvoyés vers la page de login
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // création d'une liste d'articles vide
+        $articles = [];
+
+        if ($this->isGranted('ROLE_EDITOR')) {
+            // si l'utilisateur est un éditeur, il peut voir la liste complète
+            $articles = $articleRepository->findAll();
+        } elseif ($this->isGranted('ROLE_WRITER')) {
+            // si l'utilisateur est un rédacteur, il ne peut voir que ses articles
+            // récupération du compte utilsateur
+            $user = $this->getUser();
+            // récupération du profile rédacteur à partir du compte utilsateur
+            $writer = $this->writerRepository->findByUser($user);
+            // récupération des articles du profile rédacteur
+            $articles = $writer->getArticles();
+        }
+
+        return $this->render('admin_article/index.html.twig', [
+            // envoi de la liste des articles à la vue
+            'articles' => $articles,
+        ]);
+    }
+
+    #[Route('/new', name: 'app_admin_article_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ArticleRepository $articleRepository): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $articleRepository->add($article, true);
+
+            return $this->redirectToRoute('app_admin_article_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_article/new.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_article_show', methods: ['GET'])]
+    public function show(Article $article): Response
+    {
+        // les utilisateurs non authentifiés sont renvoyés vers la page de login
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // méthode alternative à la méthode denyAccessUnlessGranted()
+        // if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
+        //     // l'utilisateur n'est pas authentifié
+        //     throw new AccessDeniedException();
+        // }
+
+        if (!$this->isGranted('ROLE_EDITOR') && $this->isGranted('ROLE_WRITER')) {
+            $user = $this->getUser();
+            $writer = $this->writerRepository->findByUser($user);
+            $articles = $writer->getArticles();
+            if (!$articles->contains($article)) {
+                // le rédacteur n'est pas auteur de l'article
+                throw new AccessDeniedException();
+            }
+        }
+
+        return $this->render('admin_article/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_admin_article_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Article $article, ArticleRepository $articleRepository): Response
+    {
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $articleRepository->add($article, true);
+
+            return $this->redirectToRoute('app_admin_article_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_article/edit.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_article_delete', methods: ['POST'])]
+    public function delete(Request $request, Article $article, ArticleRepository $articleRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->request->get('_token'))) {
+            $articleRepository->remove($article, true);
+        }
+
+        return $this->redirectToRoute('app_admin_article_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
--- src/Controller/DbTestController.php
+++ src/Controller/DbTestController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\Article;
 use App\Entity\Category;
-use App\Entity\Tag;
 use App\Entity\Page;
+use App\Entity\Tag;
+use App\Repository\ArticleRepository;
+use App\Repository\EditorRepository;
+use App\Repository\UserRepository;
+use App\Repository\WriterRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,38 +18,165 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DbTestController extends AbstractController
 {
-    #[Route('/db/test', name: 'app_db_test')]
-    public function index(ManagerRegistry $doctrine): Response
+    #[Route('/db/test/fixtures', name: 'app_db_test_fixtures')]
+    public function fixtures(ManagerRegistry $doctrine): Response
     {
-        // récupération du repository des categories
+        // récupération du repository des catégories
         $repository = $doctrine->getRepository(Category::class);
-        // récupération de la liste complète de toutes les categories
+        // récupération de la liste complète de toutes les catégories
         $categories = $repository->findAll();
         // inspection de la liste
         dump($categories);
-        
+
+        // récupération du repository des tags
         $repository = $doctrine->getRepository(Tag::class);
+        // récupération de la liste complète de tous les tags
         $tags = $repository->findAll();
+        // inspection de la liste
         dump($tags);
 
+        // récupération du repository des articles
         $repository = $doctrine->getRepository(Article::class);
+        // récupération de la liste complète de tous les articles
         $articles = $repository->findAll();
 
-        foreach ($articles as $article){
+        foreach ($articles as $article) {
+            // inspection de l'article
             dump($article);
 
+            // récupération des tags de l'article
             $tags = $article->getTags();
 
-            foreach ($tags as $tag){
+            foreach ($tags as $tag) {
+                // inspection du tag
                 dump($tag);
             }
         }
-            // récupération du repository des categories
-            $repository = $doctrine->getRepository(Page::class);
-            // récupération de la liste complète de toutes les categories
-            $pagesz = $repository->findAll();
-            dump($pages);
-            
+
+        // récupération du repository des pages
+        $repository = $doctrine->getRepository(Page::class);
+        // récupération de la liste complète de toutes les pages
+        $pages = $repository->findAll();
+        // inspections de la liste des pages
+        dump($pages);
+
+        exit();
+    }
+
+    #[Route('/db/test/orm', name: 'app_db_test_orm')]
+    public function orm(ManagerRegistry $doctrine): Response
+    {
+        // récupération du repository de l'entité Tag
+        $repository = $doctrine->getRepository(Tag::class);
+
+        // récupération de tous les objets de type Tag
+        $tags = $repository->findAll();
+        dump($tags);
+
+        // récupération d'un objet à partir de son id
+        $id = 1;
+        $tag = $repository->find($id);
+        dump($tag);
+
+        // récupération de plusieurs objets à partir de son name
+        $tags = $repository->findBy(['name' => 'carné']);
+        dump($tags);
+
+        // récupération d'un objet à partir de son name
+        $tag = $repository->findOneBy(['name' => 'carné']);
+        dump($tag);
+
+        // récupération de l'Entity Manager
+        $manager = $doctrine->getManager();
+
+        if ($tag) {
+            // suppression d'un objet
+            $manager->remove($tag);
+            $manager->flush();
+        }
+
+        // récupération d'un objet à partir de son id
+        $id = 7;
+        $tag = $repository->find($id);
+        dump($tag->getName());
+
+        // modification d'un objet
+        $tag->setName('Foo bar baz');
+        dump($tag->getName());
+
+        // enregistrement de la modification dans la BDD
+        $manager->flush();
+
+        // création d'un nouvel objet
+        $tag = new Tag();
+        $tag->setName('le dernier tag');
+        dump($tag);
+
+        // demande d'enregistrement de l'objet dans la BDD
+        $manager->persist($tag);
+        $manager->flush();
+        dump($tag);
+
+        exit();
+    }
+
+    #[Route('/db/test/repository', name: 'app_db_test_repository')]
+    public function repository(
+        ArticleRepository $articleRepository,
+        EditorRepository $editorRepository,
+        UserRepository $userRepository,
+        WriterRepository $writerRepository
+    ): Response
+    {
+        $articles = $articleRepository->findAllSorted();
+        dump($articles);
+
+        $articles = $articleRepository->findByKeyword('plat');
+        dump($articles);
+
+        $writer = $writerRepository->find(1);
+        $user = $writer->getUser();
+        // force doctrine à lancer le lazy loading
+        $user->getEmail();
+        dump($user);
+
+        $editor = $editorRepository->find(1);
+        $user = $editor->getUser();
+        // force doctrine à lancer le lazy loading
+        $user->getEmail();
+        dump($user);
+
+        $user1 = $userRepository->find(1);
+        // force doctrine à lancer le lazy loading
+        $user1->getEmail();
+        dump($user1);
+
+        $user2 = $userRepository->find(2);
+        // force doctrine à lancer le lazy loading
+        $user2->getEmail();
+        dump($user2);
+
+        $editor = $editorRepository->findByUser($user1);
+        dump($editor);
+        $editor = $editorRepository->findByUser($user2);
+        dump($editor);
+
+        $writer = $writerRepository->findByUser($user1);
+        dump($writer);
+        $writer = $writerRepository->findByUser($user2);
+        dump($writer);
+
+        $role = 'ROLE_WRITER';
+        $users = $userRepository->findByRole($role);
+        dump($users);
+
+        $articles = $articleRepository->findByPublishedAtIsNull();
+        dump($articles);
+
+        $date = DateTime::createFromFormat('Y-m-d H:i:s', '2022-06-30 00:00:00');
+        $articles = $articleRepository->findByPublishedAtBefore($date);
+        dump($articles);
+
         exit();
     }
 }
